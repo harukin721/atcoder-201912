10b8e7b684564414eba245e85d8400daa99c11f6
harukin <harukin@pepabo.com>
2024-09-25 22:57:13 +0900

    A: Length check

--- A/main.go
+++ A/main.go
@@ -9,6 +9,12 @@ func main() {
 	var S string
 	fmt.Scan(&S)
 
+	// 標準入力で受け取った長さが3であるかを確認
+	if len(S) != 3 {
+		fmt.Println("error")
+		return
+	}
+
 	// strconv.Atoi() は、文字列を整数に変換する関数
 	if num, err := strconv.Atoi(S); err == nil {
 		fmt.Println(num * 2)
